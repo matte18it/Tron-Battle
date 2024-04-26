4da6be3de210a83b0ed0ca24a20cd8ad8bd088cf
Matteo Canino <cnnmtt02p18c352y@studenti.unical.it>
2024-04-26 10:32:13 +0200

    Inizio lavoro sul  menu

--- TronBattle/src/main/java/org/application/view/GameFrame.java
+++ TronBattle/src/main/java/org/application/view/GameFrame.java
@@ -1,12 +1,34 @@
 package org.application.view;
 
+import org.application.utility.Settings;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JPanel {
+    // Attributi
+    private static final JFrame frameGame = new JFrame("Menu Principale");
+
+    //Metodi
+    public static void launchMenu() {
+        settings(); // metodo per settare i settings della finestra
+        frameGame.setVisible(true);
+    }
+
     public static void launch() {
-        JFrame frame = new JFrame("Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1280, 660);
-        frame.setVisible(true);
+        frameGame.setVisible(true);
+    }
+
+    private static void settings() {
+        //Con questo setto la dimensione e la posizione della finestra di gioco
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenDimension = toolkit.getScreenSize();
+        frameGame.setSize(1280, 660);
+        // Mettiamo la finestra al centro dello schermo
+        int x = (screenDimension.width - frameGame.getWidth())/2;
+        int y = (screenDimension.height - frameGame.getHeight())/2;
+        frameGame.setLocation(x, y);
+
+        frameGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
